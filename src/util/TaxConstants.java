244d8a4ff7f3d9035d06e582be9e0d7096737019
ds <dhanasekar.raju@outlook.com>
2025-08-11 09:05:16 +0530

    seperated constants for Tax Rate

--- src/util/TaxConstants.java
+++ src/util/TaxConstants.java
@@ -6,4 +6,8 @@ public class TaxConstants {
     public static final BigDecimal BASIC_TAX_RATE = new BigDecimal("0.10");
     public static final BigDecimal IMPORT_TAX_RATE = new BigDecimal("0.05");
     public static final BigDecimal ROUNDING_FACTOR = new BigDecimal("0.05");
+
+    private TaxConstants() {
+        throw new UnsupportedOperationException("This is a util class and cannot be instantiated");
+    }
 }
